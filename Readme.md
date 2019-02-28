166b8db310c69ad2db3c0fe5d5bbee2747a29576
Jesse <themotleydev@gmail.com>
2019-02-28 10:13:32 +0100

    updating the file

---
+++ Readme.md
@@ -0,0 +1,33 @@
+# ConfQL Schema Stitching Demo
+
+💻🧵
+
+## Explanation
+GraphQL schema stitching is an excellent way to enhance APIs with a wealth of extra data. The concept is simple and the execution is straight forward. The idea is simply to access two or more APIs in one.
+
+The execution involves four steps.
+
+1. Introspect the remote APIs. Finding out what schema structure you have to work with.
+2. Handle type name collisions.
+3. Associate which fields get added to which types.
+4. Resolve the data.
+
+## Stitching Gotchas
+It's not always a good idea to stitch your schemas together. Here are some reasons why you might not want to stitch.
+
+* The endpoints are not versioned or reliable and might change on you without proper notice.
+* One endpoint for all your data also means one endpoint to take down the project.
+* Difference in TTL for your data.
+
+## Stitch _Some_ of the Things!
+Withe the gotchas in mind, stitching is a great way to combine multiple data sets into a single distributable, explorable and maintainable API. Particularly in API design for MVP projects or one off sites, it's a great way to get the developers up and running, fast.
+
+## Requirements
+
+API | Requirements
+------------ | -------------
+Geocode API | You need to build your own Geocode API. You can run this repo locally, or host it with Zeit Now.
+Content API | Feel free to use the one we have provided! No limits!
+Yelp API | Register for a yelp authentication key and add it to a .env file. `YELP_TOKEN=...`
+
+Happy coding!
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 import { ApolloServer } from 'apollo-server';
 import { HttpLink } from 'apollo-link-http';
 import {
@@ -12,25 +13,24 @@ import {
 import fetch from 'node-fetch';
 
 // Our APIs
-const WEATHER_API =
-  'https://geocodeql.now.sh';
-const MY_API =
-  'https://api-euwest.graphcms.com/v1/cjo9rdfea63cx01dgfkqtrcad/master'
-
+const WEATHER_API = 'http://localhost:9000';
+const MY_API = 'https://api-euwest.graphcms.com/v1/cjslyzurw378n01bs1c3ip1ds/master';
+const YELP_API = 'https://api.yelp.com/v3/graphql';
 
 // Server Function
 async function run(){
 
-
 /* First we need to fetch our remote APIs,
 inspect their content and then apply the use
 Apollo to merge their schemas. */
-  const createRemoteSchema = async (uri) => {
+  const createRemoteSchema = async (uri,settings) => {
+    const config = {uri: uri, fetch, ...settings}
   try {
-    const link = new HttpLink({uri: uri, fetch});
+    const link = new HttpLink(config);
     // Introspection is what gives us
     //the self documenting magic of GraphQL
     const schema = await introspectSchema(link);
+    
     return makeRemoteExecutableSchema({
       schema,
       link,
@@ -43,6 +43,12 @@ Apollo to merge their schemas. */
 // Process the APIs
 const remoteWeatherAPI = await createRemoteSchema(WEATHER_API)
 const myRemoteAPI = await createRemoteSchema(MY_API)
+const remoteYelp = await createRemoteSchema(YELP_API, {
+  credentials: "include",
+  headers: {
+    "Authorization": `Bearer ${process.env.YELP_TOKEN}`
+  }
+})
 
 /* Here I rename some more collisions around the name 'location'
 - but I also remove all non query operations just to keep things
@@ -57,11 +63,28 @@ const myTransformedAPI = transformSchema(myRemoteAPI, [
     name === 'location' ? `GCMS_${name}` : name),
 ]);
 
+
+const yelpTransformedAPI = transformSchema(remoteYelp, [
+  new FilterRootFields(
+    (operation, rootField) => operation === 'Query'
+  ),
+  new RenameTypes((name) =>
+    name === 'Location' ? `Place` : name),
+  new RenameRootFields((operation, name) =>
+    name === 'location' ? `place` : name),
+]);
+
+
 /* This is an important step, it lets us tell the schema
 which fields should be connected between the schemas. */
 const linkTypeDefs = `
-  extend type Venue {
+  extend type Conference {
     location: Location
+  }
+
+  extend type Location {
+    hotels: Businesses
+    food: Businesses
   }
 `;
 
@@ -73,21 +96,20 @@ const schema = mergeSchemas({
     schemas: [
       remoteWeatherAPI,
       myTransformedAPI,
+      yelpTransformedAPI,
       linkTypeDefs,
     ],
 
     // Resolve them here
     resolvers: {
-
-      
       // Which type gets the new fields
-      Venue: {
+      Conference: {
         
         // Which field
         location: {
           
           // What's the 'value' we will pass in from our existing Schema
-          fragment: `... on Venue { city, country }`,
+          fragment: `... on Conference { city, country, startDate }`,
           resolve(response, args, context, info) {
             return info.mergeInfo.delegateToSchema({
               
@@ -104,10 +126,70 @@ const schema = mergeSchemas({
               // from our query above which is a JSON response?
               args: {
                 place: `${response.city}, ${response.country}`,
+                date: `${response.startDate}`
               },
               context,
               info,
               transforms: myTransformedAPI.transforms
+            });
+          },
+        },
+      },
+
+      Location: {
+        // Which field
+        hotels: {
+          
+          // What's the 'value' we will pass in from our existing Schema
+          fragment: `... on Conference { city, country }`,
+          resolve(response, args, context, info) {
+            return info.mergeInfo.delegateToSchema({
+              
+              // Which Schema returns the data for the field above
+              schema: remoteYelp,
+              
+              // What's the operation it should perform 
+              operation: 'query',
+              
+              // What field is is querying ON the delegated Schema?
+              fieldName: 'search',
+              
+              // What arguments do we pass in -
+              // from our query above which is a JSON response?
+              args: {
+                location: `${response.city}, ${response.country}`,
+                term: "Hotels"
+              },
+              context,
+              info,
+              transforms: yelpTransformedAPI.transforms
+            });
+          },
+        },
+        food: {
+          // What's the 'value' we will pass in from our existing Schema
+          fragment: `... on Conference { city, country }`,
+          resolve(response, args, context, info) {
+            return info.mergeInfo.delegateToSchema({
+              
+              // Which Schema returns the data for the field above
+              schema: remoteYelp,
+              
+              // What's the operation it should perform 
+              operation: 'query',
+              
+              // What field is is querying ON the delegated Schema?
+              fieldName: 'search',
+              
+              // What arguments do we pass in -
+              // from our query above which is a JSON response?
+              args: {
+                location: `${response.city}, ${response.country}`,
+                term: "Burgers"
+              },
+              context,
+              info,
+              // transforms: myTransformedAPI.transforms
             });
           },
         }
@@ -116,9 +198,10 @@ const schema = mergeSchemas({
     }
   });
 
+
 // Server Code - the end.
 const server = new ApolloServer({ schema });
-  server.listen().then(({ url }) => {
+  server.listen(8000).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
 
